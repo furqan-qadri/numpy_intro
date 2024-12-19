d09c4f7483dd04d916443198f462affb5a08e04d
furqan-qadri <furqaanqadri@gmail.com>
2024-12-19 20:37:15 +0000

    add methods

--- array_basics.py
+++ array_basics.py
@@ -16,5 +16,38 @@ array2=np.array([6,7,8,9,10])
 
 # iterate through each and change the data type of each value
 
-for x in np.nditer(array1, flags=['buffered'], op_dtypes=['S']):
-    print(type(x))
+# for x in np.nditer(array1, flags=['buffered'], op_dtypes=['S']):
+    # print((x))
+    
+array3=np.array_split(array1,3)
+array4 = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
+
+# print(array3)
+# array4=np.vsplit(array4,2)
+# print(array4)
+
+
+x=np.where(array2==7)
+# print(x)
+
+array7=np.arange(100,200,10)
+array7=array7.reshape(2,5)
+# print(array7)
+
+
+# Exercise 4: Return array of odd rows and even columns from below numpy array
+sampleArray = np.array([[3 ,6, 9, 12], [15 ,18, 21, 24], 
+[27 ,30, 33, 36], [39 ,42, 45, 48], [51 ,54, 57, 60]])
+
+# print(sampleArray)
+
+# Exercise 5: Create a result array by adding the following two NumPy arrays. Next, modify the result array by calculating the square of each element
+
+arrayOne = np.array([[5, 6, 9], [21 ,18, 27]])
+arrayTwo = np.array([[15 ,33, 24], [4 ,7, 1]])
+
+
+arrayThree=arrayOne+(arrayTwo)
+arrayThree=arrayThree**2
+arrayFlat=arrayThree.ravel()
+print(arrayOne.ravel())
