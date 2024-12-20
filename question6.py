6bb60448225f75b412d514f4a7eeba5a5c8c0dba
furqan-qadri <furqaanqadri@gmail.com>
2024-12-20 19:38:16 +0000

    add questions

---
+++ question6.py
@@ -0,0 +1,11 @@
+import numpy as np
+array1=np.arange(10,34).reshape(8,3)
+split_array=np.split(array1,4)
+# print(split_array)
+
+
+sampleArray = np.array([[34,43,73],[82,22,12],[53,94,66]]) 
+print(sampleArray,1)
+
+
+
